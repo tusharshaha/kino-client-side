eb096286e56d9fa1d2ba1a438e86b6179b2d8f70
tusharshaha <tusharshaha128@gmail.com>
2022-01-18 19:28:01 +0600

    added hot collection button in top banner

--- Components/TopBanner.js
+++ Components/TopBanner.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
 import bannerImg from '/public/slider-hero-img.png'
-import {AiOutlinePlus} from 'react-icons/ai'
 import styles from '../styles/TopBanner.module.css'
 const TopBanner = () => {
     return (
@@ -10,11 +9,13 @@ const TopBanner = () => {
                 <div className='flex flex-col justify-center'>
                     <span className='block uppercase bg-red-500 text-white w-48 font-bold rounded-lg py-0.5 mb-3 flex justify-center'>COVID -19 PRODUCTS</span>
                     <h1>Face Mask Thermometer</h1>
-                    <div className='text-slate-500 mt-3 pl-10 border-l-4 border-red-500'>
+                    <div className='text-slate-500 mt-4 pl-10 border-l-4 border-red-500'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est velit laboriosam obcaecati qui veritatis aut consequuntur. Culpa maiores autem necessitatibus?
                     </div>
-                    <div className='mt-5 flex'>
-                        <button className='flex cus-btn mr-2 uppercase'>Shop Now <AiOutlinePlus className='ml-2'/></button>
+                    <div className='mt-10 flex'>
+                        <button className='flex cus-btn pt-1.5 mr-4 uppercase transition duration-300 hover:bg-red-500'>Shop Now <span className='ml-2'>+</span></button>
+
+                        <button className={`${styles.btn_after} uppercase transition duration-300 hover:text-sky-500`}>Hot Collection<span className='ml-2'>+</span></button>
                     </div>
                 </div>
                 <div>
