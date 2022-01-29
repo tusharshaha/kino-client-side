b0b3917e339c1960dc8b3e8dbb580d41992d2916
tusharshaha <tusharshaha128@gmail.com>
2022-01-29 22:36:21 +0600

    short product added

---
+++ Components/Home/ShortProduct.js
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import React from 'react';
+import cat1 from '/public/cat-1.png'
+import cat2 from '/public/cat-2.png'
+import cat3 from '/public/cat-3.png'
+import Styles from '../../styles/ShortProduct.module.css'
+const ShortProduct = () => {
+    const products = [
+        { img: cat1, category: 'Digital Meter', name: 'Blood Pressure Meter', prevPrice: '155.99', curPrice: '109.99', bg: 'bg-blue-50' },
+        { img: cat2, category: 'Medical Equipment', name: 'Inhaler Machine', prevPrice: '250.99', curPrice: '239.99', bg: 'bg-purple-50' },
+        { img: cat3, category: 'Home Accessories', name: 'Hand Sanitizer', prevPrice: '140.99', curPrice: '90.99', bg: 'bg-sky-50' }
+    ]
+    return (
+        <div className='cus-container my-6'>
+            <div className='grid grid-cols-3 gap-6'>
+                {
+                    products.map((p, i) => <div className={`text-center relative p-8 ${p.bg}`} key={i}>
+                        <div className={Styles.hot}>
+                            <h5>Hot</h5>
+                        </div>
+                        <h5>{p.category}</h5>
+                        <h4 className='font-bold mt-3 mb-6'>{p.name}</h4>
+                        <Image src={p.img} width={250} height={250} alt='short product image' />
+                        <div className='mt-6'>
+                            <span className='text-slate-500 mr-3'><strike>&#36;{p.prevPrice}</strike></span>
+                            <span className='text-red-500 text-2xl font-bold'>&#36;{p.curPrice}</span>
+                        </div>
+                        <button className='bg-sky-500 px-8 py-3 text-white font-bold rounded-full mt-6 hover:bg-red-500 transition duration-300'>Buy Now +</button>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ShortProduct;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import DealsTimer from '../Components/Home/DealsTimer'
 import FeatureProduct from '../Components/Home/FeatureProduct'
 import NewsBlogs from '../Components/Home/NewsBlogs'
 import ShortCategory from '../Components/Home/ShortCategory'
+import ShortProduct from '../Components/Home/ShortProduct'
 import TopBanner from '../Components/Home/TopBanner'
 import UserReview from '../Components/Home/UserReview'
 import Footer from '../Shared/Footer'
@@ -14,15 +15,18 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
+    {/* this is header  */}
       <Head>
         <title>Kino | Home</title>
       </Head>
+    {/* this is main body  */}
       <main>
         <TopBanner></TopBanner>
         <ShortCategory></ShortCategory>
         <FeatureProduct></FeatureProduct>
         component break
         <DealsTimer></DealsTimer>
+        <ShortProduct></ShortProduct>
         <UserReview></UserReview>
         <NewsBlogs></NewsBlogs>
         <Brands></Brands>
