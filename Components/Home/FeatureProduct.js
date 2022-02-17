9453ee1f8252d29bec45d68ee350ece7a22c2f42
tusharshaha <tusharshaha128@gmail.com>
2022-02-17 22:10:50 +0600

    fixed skeleton issue

--- Components/Home/FeatureProduct.js
+++ Components/Home/FeatureProduct.js
@@ -42,9 +42,7 @@ const FeatureProduct = (props) => {
                 :
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {
-                        Array.from({ length: 6 }).map((_, i) => {
-                            <Skeleton key={i} />
-                        })
+                        Array.from({ length: 6 }).map((_, i) => <Skeleton key={i} />)
                     }
                 </div>
             }
--- Components/Home/ShowProducts.js
+++ Components/Home/ShowProducts.js
@@ -28,9 +28,7 @@ const ShowProducts = () => {
                 :
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
                     {
-                        Array.from({ length: 8 }).map((_, i) => {
-                            <Skeleton key={i} />
-                        })
+                        Array.from({ length: 8 }).map((_, i) => <Skeleton key={i}/>)
                     }
                 </div>
             }
