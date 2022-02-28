a0adf6f1de0729ec710a16fbd2275f064cf5ca5c
tusharshaha <tusharshaha128@gmail.com>
2022-02-28 13:43:36 +0600

    edited related product section

--- Components/Product/RelatedProduct.js
+++ Components/Product/RelatedProduct.js
@@ -8,9 +8,9 @@ const RelatedProduct = ({related}) => {
             <div className='border-b-2 pb-4 border-slate-300'>
                 <h4 className='font-medium'>Related Products</h4>
             </div>
-            <div className='grid grid-cols-3 mt-12 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 gap-6'>
                 {
-                    related.map(p => <Product key={p._id} product={p} mode="show product"/>)
+                    related.slice(0, 3).map(p => <Product key={p._id} product={p} mode="show product"/>)
                 }
             </div>
         </div>
