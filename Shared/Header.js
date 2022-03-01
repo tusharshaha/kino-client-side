c76d0a8538e9751a7e11f67ccea17f9b5c57f62a
tusharshaha <tusharshaha128@gmail.com>
2022-03-01 11:04:22 +0600

    added dashboard menu

--- Shared/Header.js
+++ Shared/Header.js
@@ -2,15 +2,19 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { BsSuitHeartFill, BsHeart } from 'react-icons/bs'
 import { AiOutlinePhone, AiOutlineShoppingCart } from 'react-icons/ai'
-import { FiLock, FiSearch, FiMenu} from 'react-icons/fi'
+import { FiLock, FiSearch, FiMenu } from 'react-icons/fi'
 import { FaRegUserCircle, FaBriefcaseMedical } from 'react-icons/fa'
+import { MdDashboardCustomize } from 'react-icons/md'
 import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.css'
 import Sidebar from '../Components/Home/Sidebar';
+import useAuth from '../Hooks/useAuth';
+
 const Header = () => {
     const router = useRouter();
     const [sideToggle, setSideToggle] = useState(false);
     const [navToggle, setNavToggle] = useState(false);
+    const { user } = useAuth();
     return (
         <>
             {/* this is navbar is user navbar */}
@@ -23,14 +27,24 @@ const Header = () => {
                     </span>
                 </div>
                 <div className='flex items-center text-slate-400'>
-                    <button onClick={() => router.push('/my_account')} className='flex items-center'>
-                        <FiLock className='mr-2' />
-                        <span className='font-bold hover:text-sky-500 transition duration-3'>Login</span>
-                    </button>
-                    <button onClick={() => router.push('/my_account')} className='flex items-center mx-3'>
-                        <FaRegUserCircle className='mr-2' />
-                        <span className='font-bold hover:text-sky-500 transition duration-3'>Sigh Up</span>
-                    </button>
+                    {user.email ?
+                        <button onClick={() => router.push('/my_account')} className='flex items-center mr-3'>
+                            <MdDashboardCustomize className='mr-1 text-[17px]' />
+                            <span className='font-bold hover:text-sky-500 transition duration-3'>Dashboard</span>
+                        </button>
+                        :
+                        <>
+                            <button onClick={() => router.push('/my_account')} className='flex items-center'>
+                                <FiLock className='mr-2' />
+                                <span className='font-bold hover:text-sky-500 transition duration-3'>Login</span>
+                            </button>
+                            <button onClick={() => router.push('/my_account')} className='flex items-center mx-3'>
+                                <FaRegUserCircle className='mr-2' />
+                                <span className='font-bold hover:text-sky-500 transition duration-3'>Sigh Up</span>
+                            </button>
+                        </>
+
+                    }
                     <button onClick={() => router.push('/wishlist')} className='flex items-center'>
                         <BsHeart className='mr-2' />
                         <span className='font-bold hover:text-sky-500 transition duration-3'>Wishlist</span>
@@ -40,7 +54,7 @@ const Header = () => {
             {/* this is shop navbar  */}
             <nav className='py-3 px-2 nav-container sticky top-0 shadow-md bg-white z-10'>
                 <div className='nav-container relative flex items-center justify-between'>
-                    <h3 className='logo text-sky-500'><FaBriefcaseMedical className='mr-2'/> Kino</h3>
+                    <h3 className='logo text-sky-500'><FaBriefcaseMedical className='mr-2' /> Kino</h3>
                     <ul className={`${!navToggle ? styles.shop_menu : styles.show_menu}  text-slate-400 font-bold`}>
                         <li>
                             <Link href='/'><a className='transition duration-300 hover:text-sky-500'>Home</a></Link>
@@ -64,23 +78,23 @@ const Header = () => {
                             </div>
                         </li>
                         <li>
-                            <button className='text-2xl' onClick={()=>setSideToggle(!sideToggle)}><FiMenu /></button>
+                            <button className='text-2xl' onClick={() => setSideToggle(!sideToggle)}><FiMenu /></button>
                         </li>
                         <li>
-                            <button onClick={()=>router.push('/cart')} className='flex cus-btn py-2'>
-                                <AiOutlineShoppingCart className='text-2xl'/>
+                            <button onClick={() => router.push('/cart')} className='flex cus-btn py-2'>
+                                <AiOutlineShoppingCart className='text-2xl' />
                                 <div className='bg-white mx-2 px-2 rounded-full text-blue-600'>0</div> My Cart
                             </button>
                         </li>
                     </ul>
                     {/* toggle button  */}
-                    <button onClick={()=> setNavToggle(!navToggle)} className={`${styles.menu} text-2xl text-slate-500`}>
-                        <FiMenu/>
+                    <button onClick={() => setNavToggle(!navToggle)} className={`${styles.menu} text-2xl text-slate-500`}>
+                        <FiMenu />
                     </button>
                 </div>
             </nav>
 
-            <Sidebar setToggle={setSideToggle} toggle={sideToggle}/>
+            <Sidebar setToggle={setSideToggle} toggle={sideToggle} />
         </>
     );
 };
