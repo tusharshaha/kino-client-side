594bccf8c9442a6699cda9d8b50b2bc2a6243d86
tusharshaha <tusharshaha128@gmail.com>
2022-03-02 12:54:29 +0600

    created cart and wishlist logic

--- Hooks/useStore.js
+++ Hooks/useStore.js
@@ -1,18 +1,50 @@
+const { default: Swal } = require("sweetalert2");
+
 // get stored prouduct
-const getCart = () => JSON.parse(localStorage.getItem("cart"));
-// add product to store
-const addToCart = (product) => {
-    const cart = getCart();
+const getStore = (store = "cart") => JSON.parse(localStorage.getItem(store) || "[]");
+
+// update Store
+const updateStore = (store = "cart", productId) => {
+    localStorage.setItem(store, JSON.stringify(productId));
+}
+
+// add product to cart
+const addToCart = (productId, quantity) => {
+    const cart = getStore("cart");
+    let product = {};
     if (!cart) {
-        localStorage.setItem('cart', JSON.stringify(product));
+        product[id] = 1
     } else {
-        if (cart[product]) {
-            cart[product] = 1;
+        product = cart;
+        if (!cart[productId]) {
+            product[productId] = 1;
         } else {
-            cart[product] += 1;
+            product[productId] += quantity || 1;
         }
     }
+    updateStore("cart", product);
 }
-const removeFromCart = () => {
+const addToWishlit = (productId, Date) => {
+    const wishlist = getStore("wishlist");
+    let product = {};
+    if (!wishlist) {
+        product[productId] = Date;
+    } else {
+        product = wishlist;
+        if (!wishlist[productId]) {
+            product[productId] = Date;
+        } else {
+            return Swal.fire({
+                icon: "warning",
+                title: "Product Already Added to Cart!"
+            })
+        }
+    }
+    updateStore("wishlist", product);
+}
+// clear all from localstorage
+const clearStore = (store) => {
+    localStorage.removeItem(store);
+}
 
-}
+export { getStore, clearStore, addToCart, addToWishlit }
