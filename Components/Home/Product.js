4a6ce9581152958f1abc2d5a8f0f35a3c7604891
tusharshaha <tusharshaha128@gmail.com>
2022-03-02 21:49:34 +0600

    created reducer for store

--- Components/Home/Product.js
+++ Components/Home/Product.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { FiHeart, FiSearch } from 'react-icons/fi'
 import { MdAddShoppingCart } from 'react-icons/md'
+import useStore from '../../Hooks/useStore';
 import styles from '../../styles/Home/Product.module.css'
 const Product = ({ product, mode }) => {
     const router = useRouter();
@@ -12,6 +13,8 @@ const Product = ({ product, mode }) => {
         return parseInt(Math.ceil(calcDiscount));
     }
     const discount = String(showDiscount());
+    const { addToWishlit, addToCart, cartNum } = useStore();
+    console.log(cartNum)
     return (
         <div className={`${styles.shop_card} ${mode === 'show product' && styles.show} text-center`}>
             <div className='relative'>
@@ -24,11 +27,11 @@ const Product = ({ product, mode }) => {
                 <img src={product?.img} height='100%' width='100%' alt='product img' />
 
                 <div className={`${styles.shopping} flex gap-3 justify-center items-center`}>
-                    <button className='cus-btn p-4 uppercase transition duration-300 hover:bg-red-500'><FiHeart /></button>
+                    <button className='product-btn'><FiHeart /></button>
 
-                    <button className='cus-btn p-4 uppercase transition duration-300 hover:bg-red-500'><MdAddShoppingCart /></button>
+                    <button onClick={()=>addToWishlit(product._id, 'sdf')} className='product-btn'><MdAddShoppingCart /></button>
 
-                    <button onClick={() => { router.push(`/products/${product._id}`) }} className='cus-btn p-4 uppercase transition duration-300 hover:bg-red-500'><FiSearch /></button>
+                    <button onClick={() => { router.push(`/products/${product._id}`) }} className='product-btn'><FiSearch /></button>
                 </div>
             </div>
             <div className='mt-6 font-bold pb-8'>
--- Hooks/useStore.js
+++ Hooks/useStore.js
@@ -1,50 +1,23 @@
-const { default: Swal } = require("sweetalert2");
+import React, { useState } from 'react';
 
-// get stored prouduct
-const getStore = (store = "cart") => JSON.parse(localStorage.getItem(store) || "[]");
-
-// update Store
-const updateStore = (store = "cart", productId) => {
-    localStorage.setItem(store, JSON.stringify(productId));
-}
-
-// add product to cart
-const addToCart = (productId, quantity) => {
-    const cart = getStore("cart");
-    let product = {};
-    if (!cart) {
-        product[id] = 1
-    } else {
-        product = cart;
-        if (!cart[productId]) {
-            product[productId] = 1;
-        } else {
-            product[productId] += quantity || 1;
-        }
+const useStore = () => {
+    // get stored prouduct
+    const getStore = (store = "cart") => JSON.parse(localStorage.getItem(store) || "[]");
+    // setting cart number
+    const [cartNum, setCartNum] = useState(getStore("cart").length);
+    // add product to cart
+    const addToCart = (arr) => {
+        localStorage.setItem('cart', JSON.stringify(arr));
     }
-    updateStore("cart", product);
-}
-const addToWishlit = (productId, Date) => {
-    const wishlist = getStore("wishlist");
-    let product = {};
-    if (!wishlist) {
-        product[productId] = Date;
-    } else {
-        product = wishlist;
-        if (!wishlist[productId]) {
-            product[productId] = Date;
-        } else {
-            return Swal.fire({
-                icon: "warning",
-                title: "Product Already Added to Cart!"
-            })
-        }
+    const addToWishlit = (arr) => {
+        localStorage.setItem('wishlist', JSON.stringify(arr));
     }
-    updateStore("wishlist", product);
-}
-// clear all from localstorage
-const clearStore = (store) => {
-    localStorage.removeItem(store);
-}
+    // clear all from localstorage
+    const clearStore = (store) => {
+        localStorage.removeItem(store);
+    }
 
-export { getStore, clearStore, addToCart, addToWishlit }
+    return { addToCart, addToWishlit, clearStore, cartNum, setCartNum }
+};
+
+export default useStore;
