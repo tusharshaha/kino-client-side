e679933042deaef3869f4587814e6c633d9b3785
tusharshaha <tusharshaha128@gmail.com>
2022-09-13 19:27:29 +0600

    created api for review

---
+++ pages/api/v1/review.js
@@ -0,0 +1,25 @@
+import Review from "../../../models/Review.model";
+
+export default async function review(req, res) {
+    try {
+        if (req.method === "GET") {
+            const reviews = await Review.find({});
+            res.status(200).json({
+                success: true,
+                messages: "success",
+                data: reviews
+            });
+        } else if (req.method === "POST") {
+            await Review.create(req.body);
+            res.status(200).json({
+                success: true,
+                messages: "success",
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            error: "Internal server error!"
+        })
+    }
+}
