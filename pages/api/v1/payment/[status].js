77eb0a812e90b3eb50f18751782a4f4fab407788
tusharshaha <tusharshaha128@gmail.com>
2022-12-22 20:12:25 +0600

    updated url

--- pages/api/v1/payment/[status].js
+++ pages/api/v1/payment/[status].js
@@ -9,12 +9,12 @@ export default async function paymentStatus(req, res) {
             if (req.query.status === "fail") {
                 const tran_id = req.body.tran_id
                 await Order.findOneAndDelete({ tran_id })
-                res.redirect(200, "https://kino-one.vercel.app/checkout")
+                res.redirect(200, `https://${process.env.VERCEL_URL}/checkout`)
             }
             if (req.query.status === "cancel") {
                 const tran_id = req.body.tran_id
                 await Order.findOneAndDelete({ tran_id })
-                res.redirect(200, "https://kino-one.vercel.app/checkout")
+                res.redirect(200, `https://${process.env.VERCEL_URL}/checkout`)
             }
         }
     } catch (err) {
